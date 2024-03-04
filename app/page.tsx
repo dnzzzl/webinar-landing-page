9c268aae87948e73cb344606dfb4b54edf89e192
dnzzzl <Denzel2603@gmail.com>
2024-03-04 09:44:23 -0400

    Implementar componente de dialogo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import {Label} from '@/components/ui/label'
+import { Input } from "@/components/ui/input";
 import { BarChartBig, Clock, CalendarCheck } from "lucide-react";
 import clixLogoTransparentBg from "@/public/clix-logo-no-bg.png"
 import scrumLogoWhiteTransparentBg from "@/public/Scrum-Network-Logo-White.webp"
-import redBg from "@/public/6CaiUV.jpg"
+//import redBg from "@/public/6CaiUV.jpg"
 import Image from "next/image";
-import Link from "next/link";
+//import Link from "next/link";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => {
+    setOpen(!open)
+  }
+
   return (
     <>
       <main className="dark bg-scroll bg-[url('/bg-7-full.jpg')] min-h-[972px] bg-cover text-primary">
@@ -44,7 +65,7 @@ export default function Home() {
           <h2 className="text-xl md:text-4xl text-center pb-4">
             Unete al Webinar de <strong>Scrum Network</strong> en Republica Dominicana
           </h2>
-          <section className="flex justify-center flex-col bg-white text-black py-16 my-8">
+          <section className="dark flex justify-center flex-col bg-background py-16 my-8">
             <div className="flex md:flex-row flex-col">
               <div className="md:w-1/2 md:text-end text-center font-semibold">
                 <p className="capitalize p-4 text-xl md:text-2xl md:max-w-xs mx-auto">
@@ -68,11 +89,36 @@ export default function Home() {
                 </ul>
               </span>
             </div>
-            <Button className="px-8 rounded-xl mx-auto relative text-lg mt-10 bg-background text-foreground hover:text-black">
-              Unete 
-            </Button>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogTrigger className="bg-primary text-primary-foreground hover:bg-primary/90 mx-auto rounded-lg p-4">Unete</DialogTrigger>
+              <DialogContent className="dark text-primary">
+                <DialogHeader>
+                  <DialogTitle>Unete a nuestro Webinar</DialogTitle>
+                  <DialogDescription>
+                    Anade tu correo electronico para que te enviemos las proximas comunicaciones de Scrum Network.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Nombre
+                    </Label>
+                    <Input id="name" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="correo" className="text-right">
+                      Correo Electronico
+                    </Label>
+                    <Input id="correo" type="email" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Aceptar</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </section>
-          <section className="flex justify-around py-10 md:text-2xl text-foreground">
+          <section className=" flex justify-around py-10 md:text-2xl text-foreground">
             <div className="p-4 flex flex-col items-center">
               <Clock className="md:w-28 md:h-28"/>
               <p className="max-w-min text-center ">11:00 AM</p>
@@ -86,13 +132,13 @@ export default function Home() {
               <p className="max-w-min text-center">Miercoles 13 de Marzo 2024</p>
             </div>
           </section>
-          <div className="w-full flex ">
-            <Button className="w-full uppercase mx-4 ">Registrate aqui, ahora</Button>
+          <div className="w-full flex  mb-2">
+            <Button className="dark w-full uppercase mx-4" onClick={() => handleClick()}>Registrate aqui, ahora</Button>
           </div>
         </div>
-        <Separator orientation="horizontal" decorative={true} className="bg-foreground my-4 p-1"/>
+        <Separator orientation="horizontal" decorative={true} className=" p-1"/>
       </main>
-      <footer className="bg-background p-20">
+      <footer className="dark bg-background text-foreground p-20">
         <div className="md:max-w-4xl mx-auto">
           footer
         </div>
